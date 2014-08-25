d6a566efd6e11b7acbd13513e8defa5456c770bd
martincz <martincz@mokeedev.com>
2014-08-25 18:50:15 +0800

    castor_windy: mokee bringup

--- mokee.mk
+++ mokee.mk
@@ -1,12 +1,12 @@
-# Inherit CM common Phone stuff.
-$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
+# Inherit MK common Phone stuff.
+$(call inherit-product, vendor/mk/config/common_full_tablet_wifionly.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/mk/config/nfc_enhanced.mk)
 
 $(call inherit-product, device/sony/castor_windy/full_castor_windy.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGP521 BUILD_FINGERPRINT=Sony/SGP511/SGP511:4.4.2/17.1.1.A.0.402/bf53dw:user/release-keys PRIVATE_BUILD_DESC="SGP511-user 4.4.2 17.1.1.A.0.402 bf53dw release-keys"
 
-PRODUCT_NAME := cm_castor_windy
+PRODUCT_NAME := mk_castor_windy
 PRODUCT_DEVICE := castor_windy
